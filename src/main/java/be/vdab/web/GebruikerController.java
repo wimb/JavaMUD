3bc2b9fbda9cf135ed98f92df0a727dcca46e40b
lanxiang <lanxiang@live.co.uk>
2013-09-30 16:01:49 +0200

    Accoutcreate JSP/Servlet --Xiang

---
+++ src/main/java/be/vdab/web/GebruikerController.java
@@ -0,0 +1,46 @@
+package be.vdab.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.entities.Gebruiker;
+import be.vdab.exceptions.GebruikerNaamAlInGebruikException;
+import be.vdab.services.GebruikerService;
+
+@Controller
+@RequestMapping("/gebruiker")
+public class GebruikerController {
+
+	private final GebruikerService gebruikerService;
+
+	@Autowired
+	public GebruikerController(GebruikerService gebruikerService) {
+		this.gebruikerService = gebruikerService;
+	}
+
+	@RequestMapping(value = "toevoegen", method = RequestMethod.GET)
+	public ModelAndView createForm() {
+		return new ModelAndView("gebruikers/toevoegen", "gebruiker", new Gebruiker());
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(@Valid Gebruiker gebruiker, BindingResult bindingResult) {
+		if (!bindingResult.hasErrors()) {
+			try {
+				gebruikerService.create(gebruiker);
+				return "redirect:/gebruikers/toegevoegd";
+			} catch (GebruikerNaamAlInGebruikException e) {
+				bindingResult.rejectValue("gebruikerNaam",
+						"GebruikerNaamAlInGebruikException");
+			}
+		}
+		return "gebruikers/toevoegen";
+	}
+
+}
