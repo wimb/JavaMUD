30e0d4b936fe18757dbffb833d2310f7d99cc531
tim.vandenlangenberg <tim.vandenlangenberg@PC24715.ad.core.vdabantwerpen.intra>
2013-10-01 11:19:40 +0200

    Updated LokatieController

--- src/main/java/be/vdab/web/LokatieController.java
+++ src/main/java/be/vdab/web/LokatieController.java
@@ -5,12 +5,15 @@
     
 package be.vdab.web;
     
+import be.vdab.entities.Karakter;
+import be.vdab.services.KarakterService;
 import be.vdab.services.LokatieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -23,18 +26,34 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @RequestMapping("/lokatie")
+@SessionAttributes("karakter")
 public class LokatieController {
     private final LokatieService lokatieService;
+    private final KarakterService karakterService;
     
     @Autowired
-    public LokatieController(LokatieService lokatieService){
+    public LokatieController(LokatieService lokatieService, 
+            KarakterService karakterService){
         this.lokatieService = lokatieService;
+        this.karakterService = karakterService;
     }
     
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView findAll(@RequestParam long lokatieId){
+    public ModelAndView read(@RequestParam long lokatieId){
         ModelAndView mav = new ModelAndView("lokatie");
         mav.addObject("lokatie", lokatieService.read(lokatieId));
+        return mav;
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView findKarakterLokatie(@RequestParam long karakterId){
+        ModelAndView mav = new ModelAndView("lokatie");
+        Karakter k = karakterService.read(karakterId);
+        if(k.getLokatie() == null){
+            k.setLokatie(lokatieService.read(1));
+        }
+        mav.addObject("lokatie", k.getLokatie());
+        mav.addObject("karakter", k);
         return mav;
     }
     
